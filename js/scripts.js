7970e3bfbac2756bb84d738c5e6cb01754631a70
Justin Peters <JMarvel@gmail.com>
2022-06-15 11:18:04 -0700

    add 2 buttons that give the user random number between 1 and 6
    Co-Authored by:Ryan DeFea <ryan.defea@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,20 @@
+//Business logic------------------------------------------
 function diceRoll (min, max) {
   min = Math.ceil(min);
   max = Math.floor(max);
   return Math.floor(Math.random() * (max - min + 1) + min);
 }
-  diceRoll(1, 6)
+
+  // diceRoll(1, 6)
+
+//UI Logic-------------------------------------------------
+$(document).ready(function() {
+  $("form#pigGame1").submit(function(event){
+  event.preventDefault();
+  $("#p1Score").html(diceRoll(1,6));
+  });
+  $("form#pigGame2").submit(function(event){
+  event.preventDefault();
+  $("#p2Score").html(diceRoll(1,6));
+  });
+});
