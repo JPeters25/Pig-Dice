4f0186582d1cce2f04bb502d85e66265f2ed28c1
ryandefea <ryan.defea@gmail.com>
2022-06-16 10:18:22 -0700

    add constructor and prototypes.

--- js/scripts.js
+++ js/scripts.js
@@ -1,53 +1,55 @@
 //Business logic------------------------------------------
-
-
-// function diceRoll (min, max) {
-//   min = Math.ceil(min);
-//   max = Math.floor(max);
-//   return Math.floor(Math.random() * (max - min + 1) + min);
-// }
 function diceRoll() {
-  this.p1Total = 0
-  this.p2Total = 0;
-  this.p1Roll = 0;
-  this.p2Roll = 0;
+  this.Total = 0;
+  this.Roll = 0;
 }
 
 let newPlayer = new diceRoll(0,0);
 let newPlayer2 = new diceRoll(0,0);
 
 diceRoll.prototype.GetRoll = function(max) {
-  this.p1Roll = Math.ceil(Math.random() * 6);
+  this.Roll = Math.ceil(Math.random() * 6);
 }
 
 diceRoll.prototype.AddRoll = function() {
-  if (this.p1Roll != 1){
-    this.p1Total += this.p1Roll;
+  if (this.Roll != 1){
+    this.Total += this.Roll;
   } else {
-
+    this.Total = this.Total;
   }
 }
+
+
+
+
+
 
   // diceRoll(1, 6)
 
 //UI Logic-------------------------------------------------
-$(document).ready(function() {
-  $("form#pigGame1").submit(function(event){
+$(document).ready(function () {
+  $("#player-one-roll").click(function(event){
   event.preventDefault();
   newPlayer.GetRoll();
   newPlayer.AddRoll();
-  $("#p1Total").text(newPlayer.p1Total);
+  $('#p1Roll').text(newPlayer.Roll)
+  $("#p1Total").text(newPlayer.Total);
   });
-  // $("form#pigGame2").submit(function(event){
-  // event.preventDefault();
-  // $("#p2Roll").text(diceRoll(1,6));
-  // });
-});
 
-$(document).ready(function () {
-  $("button#hide-score").click(function(event){
+  $("#player-two-roll").click(function(event){
+  event.preventDefault();
+  newPlayer2.GetRoll();
+  newPlayer2.AddRoll();
+  $('#p2Roll').text(newPlayer2.Roll)
+  $("#p2Total").text(newPlayer2.Total);
+  });
+
+
+  $("button#test").click(function(event){
     event.preventDefault();
     $("#p1Total").text("");
     $("#p2Total").text("");
+    $("#p1Roll").text("");
+    $("#p2Roll").text("");
   });
 });
